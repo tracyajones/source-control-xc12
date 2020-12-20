5046987bc721fbec6f0ade84a5436c823815317f
Tracy Jones <tracyajones@me.com>
2020-12-20 15:44:01 -0800

    Initial Commit

---
+++ First Project/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  First Project
+//
+//  Created by Tracy Jones on 12/20/20.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ First Project/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  First Project
+//
+//  Created by Tracy Jones on 12/20/20.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
