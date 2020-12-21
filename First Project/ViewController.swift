122d8ac208a881f4f48e97a558f5fb6f0c1e4c4e
Tracy Jones <tracyajones@me.com>
2020-12-21 11:34:41 -0800

    add new numbers method

--- First Project/ViewController.swift
+++ First Project/ViewController.swift
@@ -14,6 +14,17 @@ class ViewController: NSViewController {
 
         // Do any additional setup after loading the view.
     }
+    
+    /**
+     Adds two numbers and return sum
+     - parameter num1: the first number
+     - parameter num2: the second number
+     */
+    func addNumbers(num1: Int, num2: Int) -> Int {
+        return num1 + num2
+    }
+
+
 
     override var representedObject: Any? {
         didSet {
